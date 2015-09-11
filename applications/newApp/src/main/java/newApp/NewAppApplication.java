73a5ed661dfee9fe253ae1334897da4f1c522f11
Mike Gehard <mike.gehard@gmail.com>
2015-09-10 19:11:05 -0600

    Initial commit

---
+++ applications/newApp/src/main/java/newApp/NewAppApplication.java
@@ -0,0 +1,22 @@
+package newApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableZuulProxy
+public class NewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewAppApplication.class, args);
+    }
+
+    @RequestMapping("/one")
+    public String one(){
+        return "One from new application";
+    }
+}
---
+++ applications/oldApp/src/main/java/oldApp/OldAppApplication.java
@@ -0,0 +1,25 @@
+package oldApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OldAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldAppApplication.class, args);
+    }
+
+    @RequestMapping("/one")
+    public String one(){
+        return "One from old application";
+    }
+
+    @RequestMapping("/two")
+    public String two(){
+        return "Two from old application";
+    }
+}
